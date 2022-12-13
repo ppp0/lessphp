2ea6eb88fd56a2e88618fdb217629425c9e09081
ppp0 <philipp.pfaff@cargomedia.ch>
2022-12-13 15:56:29 +0100

    Fix casting float to int

--- lessc.inc.php
+++ lessc.inc.php
@@ -1669,7 +1669,7 @@ class lessc {
                 $width = strlen($colorStr) == 3 ? 16 : 256;
 
                 for ($i = 3; $i > 0; $i--) { // 3 2 1
-                    $t = $num % $width;
+                    $t = (int) round($num) % (int) round($width);
                     $num /= $width;
 
                     $c[$i] = $t * (256/$width) + $t * floor(16/$width);
